c19f8268fff7fe1ab5990c54186be73ec5e4ff4d
Frédéric Bessette <msfredb7@gmail.com>
2020-07-12 16:36:30 -0400

    new RectTransform extension

---
+++ UnityEngineX/TypeExtensions/RectTransformExtensions.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEngineX
+{
+    public static class RectTransformExtensions
+    {
+        public static Rect GetRectRelativeTo(this RectTransform targetRectTransform, RectTransform relativeRectTransform)
+        {
+            Rect aRect = targetRectTransform.rect;
+            Rect bRect = relativeRectTransform.rect;
+
+            Vector2 relativeSize = relativeRectTransform.InverseTransformVector(targetRectTransform.TransformVector(aRect.size));
+            Vector2 relativeMin = relativeRectTransform.InverseTransformPoint(targetRectTransform.TransformPoint(aRect.min));
+
+            return new Rect(relativeMin - bRect.min, relativeSize);
+        }
+
+        public static Rect GetScreenRect(this RectTransform rectTr)
+        {
+            var canvas = rectTr.GetComponentInParent<Canvas>();
+
+            if (canvas == null)
+                throw new Exception("No canvas found in parents.");
+
+            if(canvas.renderMode == RenderMode.WorldSpace)
+            {
+                Rect rect = rectTr.rect;
+
+                Camera camera = canvas.worldCamera;
+                if(camera == null)
+                    throw new Exception("No camera attached to canvas with 'WorldSpace' render mode.");
+
+
+                Vector2 screenMin = camera.WorldToScreenPoint(rectTr.TransformPoint(rect.min));
+                Vector2 screenMax = camera.WorldToScreenPoint(rectTr.TransformPoint(rect.max));
+
+                return new Rect(screenMin, screenMax - screenMin);
+            }
+            else
+            {
+                return rectTr.GetRectRelativeTo((RectTransform)canvas.transform);
+            }
+        }
+    }
+}
