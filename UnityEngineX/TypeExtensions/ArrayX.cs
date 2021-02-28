89b82e78df05c733a04ff17b74a76061197a7114
Frédéric Bessette <msfredb7@gmail.com>
2021-02-27 20:12:00 -0500

    ArrayX

---
+++ UnityEngineX/TypeExtensions/ArrayX.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UnityEngineX
+{
+    public class ArrayX
+    {
+        public static T[] Slice<T>(T[] array, int index, int count)
+        {
+            if (count > array.Length)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            var result = new T[count];
+
+            if(count > 0)
+            {
+                Array.Copy(array, index, result, 0, count);
+            }
+
+            return result;
+        }
+    }
+}
