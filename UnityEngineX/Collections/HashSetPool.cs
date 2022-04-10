f680b16825503eff203aa02136b84780570d30ef
Frédéric Bessette <msfredb7@gmail.com>
2022-04-10 15:20:54 -0400

    HashSetPool

---
+++ UnityEngineX/Collections/HashSetPool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngineX
+{
+    public static class HashSetPool<T>
+    {
+        public struct UsageHandle : IDisposable
+        {
+            private HashSet<T> _set;
+
+            public UsageHandle(HashSet<T> set)
+            {
+                _set = set;
+            }
+
+            public void Dispose()
+            {
+                Release(_set);
+            }
+        }
+
+        private static Queue<HashSet<T>> s_queue = new Queue<HashSet<T>>();
+
+        public static HashSet<T> Take()
+        {
+            return s_queue.Count == 0 ? new HashSet<T>() : s_queue.Dequeue();
+        }
+
+        public static UsageHandle Take(out HashSet<T> set)
+        {
+            set = Take();
+            return new UsageHandle(set);
+        }
+
+        public static void Release(HashSet<T> set)
+        {
+            set.Clear();
+            s_queue.Enqueue(set);
+        }
+    }
+}
--- UnityEngineX/Collections/ListPool.cs
+++ UnityEngineX/Collections/ListPool.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace UnityEngineX
 {
     public static class ListPool<T>
     {
+        public struct UsageHandle : IDisposable
+        {
+            private List<T> _list;
+
+            public UsageHandle(List<T> list)
+            {
+                _list = list;
+            }
+
+            public void Dispose()
+            {
+                Release(_list);
+            }
+        }
+
         private static Queue<List<T>> s_queue = new Queue<List<T>>();
 
-        public static List<T> Take()
+        public static List<T> Take() => Take(4);
+
+        public static List<T> Take(int capacity)
         {
-            return s_queue.Count == 0 ? new List<T>() : s_queue.Dequeue();
+            return s_queue.Count == 0 ? new List<T>(capacity) : s_queue.Dequeue();
         }
+
+        public static UsageHandle Take(int capacity, out List<T> list)
+        {
+            list = Take(capacity);
+            return new UsageHandle(list);
+        }
+        public static UsageHandle Take(out List<T> list) => Take(4, out list);
 
         public static void Release(List<T> list)
         {
