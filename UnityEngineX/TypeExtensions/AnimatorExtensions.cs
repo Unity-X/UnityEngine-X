2433950e45602e0349ac93a445ad3694448881b2
Frederic Bessette <msfredb7@gmail.com>
2023-06-01 17:39:22 -0400

    new Animator extensions

---
+++ UnityEngineX/TypeExtensions/AnimatorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace UnityEngineX
+{
+    public static class AnimatorExtensions
+    {
+        public static bool HasParameter(this Animator animator, string paramName)
+        {
+            foreach (AnimatorControllerParameter param in animator.parameters)
+            {
+                if (param.name == paramName)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool HasParameter(this Animator animator, int paramNameHash)
+        {
+            foreach (AnimatorControllerParameter param in animator.parameters)
+            {
+                if (param.nameHash == paramNameHash)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+}
