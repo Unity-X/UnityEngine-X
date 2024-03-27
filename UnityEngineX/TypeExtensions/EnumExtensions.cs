e45f46640892c8e35f431a86f912017088db2220
Frederic Bessette <msfredb7@gmail.com>
2024-03-27 16:28:10 -0400

    enum extension methods for allocFree HasFlag equivalents

---
+++ UnityEngineX/TypeExtensions/EnumExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace UnityEngineX
+{
+    public static class EnumExtensions
+    {
+        public static unsafe bool Contains<TEnum>(this TEnum lhs, TEnum rhs) where TEnum :
+            unmanaged, Enum
+        {
+            switch (sizeof(TEnum))
+            {
+                case 1:
+                    return (*(byte*)(&lhs) & *(byte*)(&rhs)) > 0;
+                case 2:
+                    return (*(ushort*)(&lhs) & *(ushort*)(&rhs)) > 0;
+                case 4:
+                    return (*(uint*)(&lhs) & *(uint*)(&rhs)) > 0;
+                case 8:
+                    return (*(ulong*)(&lhs) & *(ulong*)(&rhs)) > 0;
+                default:
+                    throw new Exception("Size does not match a known Enum backing type.");
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Add<TEnum>(ref this TEnum lhs, TEnum rhs) where TEnum : unmanaged, Enum
+        {
+            fixed (TEnum* lhs1 = &lhs)
+            {
+                switch (sizeof(TEnum))
+                {
+                    case 1:
+                    {
+                        var r = *(byte*)(lhs1) | *(byte*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 2:
+                    {
+                        var r = *(ushort*)(lhs1) | *(ushort*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 4:
+                    {
+                        var r = *(uint*)(lhs1) | *(uint*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 8:
+                    {
+                        var r = *(ulong*)(lhs1) | *(ulong*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    default:
+                        throw new Exception("Size does not match a known Enum backing type.");
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void Remove<TEnum>(this ref TEnum lhs, TEnum rhs) where TEnum : unmanaged, Enum
+        {
+            fixed (TEnum* lhs1 = &lhs)
+            {
+                switch (sizeof(TEnum))
+                {
+                    case 1:
+                    {
+                        var r = *(byte*)(lhs1) & ~*(byte*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 2:
+                    {
+                        var r = *(ushort*)(lhs1) & ~*(ushort*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 4:
+                    {
+                        var r = *(uint*)(lhs1) & ~*(uint*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    case 8:
+                    {
+                        var r = *(ulong*)(lhs1) & ~*(ulong*)(&rhs);
+                        *lhs1 = *(TEnum*)&r;
+                        return;
+                    }
+                    default:
+                        throw new Exception("Size does not match a known Enum backing type.");
+                }
+            }
+        }
+    }
+}
