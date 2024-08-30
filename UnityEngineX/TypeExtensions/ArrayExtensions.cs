6c8d1c62ef98e9235f00a010875b99db84ccb496
Frederic Bessette <msfredb7@gmail.com>
2024-08-30 12:11:24 -0400

    [new] sub-array method, behaves like string.Substring(startIndx, length)

--- UnityEngineX/TypeExtensions/ArrayExtensions.cs
+++ UnityEngineX/TypeExtensions/ArrayExtensions.cs
@@ -155,5 +155,28 @@ namespace UnityEngineX
             }
             return sum;
         }
+
+        public static T[] SubArray<T>(this T[] array, int startIndex, int length)
+        {
+            int num;
+            if (array == null || (num = array.Length) == 0)
+            {
+                return new T[0];
+            }
+
+            if (startIndex < 0 || length <= 0 || startIndex + length > num)
+            {
+                return new T[0];
+            }
+
+            if (startIndex == 0 && length == num)
+            {
+                return array;
+            }
+
+            T[] array2 = new T[length];
+            Array.Copy(array, startIndex, array2, 0, length);
+            return array2;
+        }
     }
 }
