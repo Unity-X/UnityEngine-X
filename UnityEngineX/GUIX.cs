9107337f7a9fec4db0353cf911f699184372f505
Frederic Bessette <msfredb7@gmail.com>
2024-11-14 13:18:05 -0500

    [fix] GUIX.DrawQuad not taking color into account

--- UnityEngineX/GUIX.cs
+++ UnityEngineX/GUIX.cs
@@ -6,12 +6,17 @@ namespace UnityEngineX
     public static class GUIX
     {
         private static Texture2D s_quad;
+        private static Color s_color;
 
         public static void DrawQuad(Rect position, Color color)
         {
             if (s_quad == null)
             {
                 s_quad = new Texture2D(1, 1);
+            }
+
+            if (s_color != color)
+            {
                 s_quad.SetPixel(0, 0, color);
                 s_quad.Apply();
             }
