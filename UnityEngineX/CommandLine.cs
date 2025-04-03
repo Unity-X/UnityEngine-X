43e334ed26676198170141fd97c375957c6692c8
Frederic Bessette <msfredb7@gmail.com>
2025-04-03 16:26:34 -0400

    rework CommandLine API

--- UnityEngineX/CommandLine.cs
+++ UnityEngineX/CommandLine.cs
@@ -1,42 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace UnityEngineX
 {
     public static class CommandLine
     {
-        static List<string> s_arguments;
-        static List<string> s_argumentGrouped;
+        static List<List<string>> s_argumentParams = new();
+        static List<string> s_argumentsGrouped;
+        static List<string> s_argumentKeys = new();
+        static bool s_init = false;
+
+        private static void Init()
+        {
+            if (s_init)
+                return;
+            s_init = true;
+            InitialiseGroupedArguments();
+        }
 
 
         /// <summary>
         /// Eg.
         /// <list type="number">
-        /// <item>-resolution</item>
-        /// <item>1920</item>
-        /// <item>1080</item>
-        /// <item>-map</item>
-        /// <item>Tundra</item>
+        /// <item>-resolution 1920 1080</item>
+        /// <item>-map Tundra</item>
         /// </list>
         /// </summary>
-        public static ReadOnlyList<string> Arguments
+        public static ReadOnlyList<string> GroupedArguments
         {
             get
             {
-                if (s_arguments == null)
-                    s_arguments = new List<string>(Environment.GetCommandLineArgs());
-                return s_arguments.AsReadOnlyNoAlloc();
+                Init();
+                return s_argumentsGrouped.AsReadOnlyNoAlloc();
             }
+        }
+
+        private static void InitialiseGroupedArguments()
+        {
+            string line = Environment.CommandLine;
+
+            int groupBegin = -1;
+            int termBegin = -1;
+            bool inQuotes = false;
+            List<string> group = new();
+            List<string> keys = new();
+            List<List<string>> parameters = new();
+            List<string> groupParams = null;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                if (c == '"')
+                {
+                    inQuotes = !inQuotes;
+                }
+                else if (c == '-' && i != 0 && line[i - 1] == ' ') // ' -'
+                {
+                    if (groupBegin > 0)
+                        FinishGroup(groupBegin, end: i - 1);
+                    groupBegin = i;
+                    termBegin = i + 1;
+                    groupParams = new();
+                }
+                else if (c == ' ' && !inQuotes)
+                {
+                    if (termBegin > 0 && line[i - 1] != ' ')
+                    {
+                        if (keys.Count == group.Count)
+                            FinishKey(termBegin, end: i);
+                        else
+                            FinishParam(termBegin, end: i);
+                    }
+                    termBegin = i + 1;
+                }
+            }
+
+            if (termBegin > 0 && line[line.Length - 1] != ' ')
+            {
+                if (keys.Count == group.Count)
+                    FinishKey(termBegin, end: line.Length);
+                else
+                    FinishParam(termBegin, end: line.Length);
+            }
+
+            if (groupBegin > 0)
+                FinishGroup(groupBegin, end: line.Length);
+
+            void FinishGroup(int begin, int end)
+            {
+                parameters.Add(groupParams);
+                group.Add(line.Substring(begin, end - begin));
+            }
+
+            void FinishParam(int begin, int end)
+            {
+                groupParams.Add(line.Substring(begin, end - begin));
+            }
+
+            void FinishKey(int begin, int end)
+            {
+                keys.Add(line.Substring(begin, end - begin));
+            }
+
+            s_argumentsGrouped = group;
+            s_argumentKeys = keys;
+            s_argumentParams = parameters;
         }
 
         public static string CompleteCommandLine => Environment.CommandLine;
 
+        public static bool TryGetAllParams(string key, out string[] parameters)
+        {
+            if (FindArgument(key, out int index))
+            {
+                parameters = s_argumentParams[index].ToArray();
+                return true;
+            }
+            parameters = default;
+            return false;
+        }
+
         public static bool TryGetInt(string key, out int value)
         {
-            if (TryGetArgumentMatchedWithValue(key, out int index))
+            if (FindArgument(key, out int index))
             {
-                return int.TryParse(Arguments[index + 1], out value);
+                if (s_argumentParams[index].Count > 0)
+                    return int.TryParse(s_argumentParams[index][0], out value);
             }
             value = default;
             return false;
@@ -44,9 +135,10 @@ namespace UnityEngineX
 
         public static bool TryGetFloat(string key, out float value)
         {
-            if (TryGetArgumentMatchedWithValue(key, out int index))
+            if (FindArgument(key, out int index))
             {
-                return float.TryParse(Arguments[index + 1], out value);
+                if (s_argumentParams[index].Count > 0)
+                    return float.TryParse(s_argumentParams[index][0], NumberStyles.Float, CultureInfo.InvariantCulture, out value);
             }
             value = default;
             return false;
@@ -54,23 +146,28 @@ namespace UnityEngineX
 
         public static bool TryGetString(string key, out string value)
         {
-            if (TryGetArgumentMatchedWithValue(key, out int index))
+            if (FindArgument(key, out int index))
             {
-                value = Arguments[index + 1];
-                return true;
+                if (s_argumentParams[index].Count > 0)
+                {
+                    value = s_argumentParams[index][0];
+                    return true;
+                }
             }
             value = default;
             return false;
         }
 
-        static bool TryGetArgumentMatchedWithValue(string key, out int index)
+        public static bool HasArgument(string key)
         {
-            index = Arguments.IndexOf(key);
-            if (index != -1 && index < Arguments.Count - 1)
-            {
-                return true;
-            }
-            return false;
+            return FindArgument(key, out _);
+        }
+
+        static bool FindArgument(string key, out int index)
+        {
+            Init();
+            index = s_argumentKeys.IndexOf(key);
+            return s_argumentKeys.IsValidIndex(index);
         }
 
         /// <summary>
